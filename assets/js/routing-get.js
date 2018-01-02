354f39022855f43470927b0186416d3ca921af2c
Oskar Pihlak <oskar.pihlak@gmail.com>
2018-01-02 11:11:33 +0200

    lol

--- assets/js/routing-get.js
+++ assets/js/routing-get.js
@@ -61,11 +61,32 @@ module.exports = (app) =>{
             });
         }).catch(err => {throw err });
     });
-    app.get('/webhook', (req, res)=>{
-        if(req.query['hub.verify_token'] === 'izApartmentStuff'){
-            res.send(req.query['hub.challenge']);
+    // Adds support for GET requests to our webhook
+    app.get('/webhook', (req, res) => {
+
+        // Your verify token. Should be a random string.
+        let VERIFY_TOKEN = "EAAH0ZBiD0bXEBANmyQ2I3QRmTIiaf79QZBn7OvJhZCE7ZCyz1Q3ZCybWz9tE4g8cBrtXmZC2DU01bd3PfWCVlPFvewCvTS28cOFEqRCZCaBphGOjNyVgZCLEiaUnYTt9v9oALqrNZC652FhZCE7ZC0jt7K2BkweF0KbiyZAaex8O0ukYUQZDZD";
+
+        // Parse the query params
+        let mode = req.query['hub.mode'];
+        let token = req.query['hub.verify_token'];
+        let challenge = req.query['hub.challenge'];
+
+        // Checks if a token and mode is in the query string of the request
+        if (mode && token) {
+
+            // Checks the mode and token sent is correct
+            if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+                // Responds with the challenge token from the request
+                console.log('WEBHOOK_VERIFIED');
+                res.status(200).send(challenge);
+
+            } else {
+                // Responds with '403 Forbidden' if verify tokens do not match
+                res.sendStatus(403);
+            }
         }
-        res.send('Wrong token');
     });
 
 
