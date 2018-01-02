1852b67fe5bd85f71404f56d8ba0044195af2906
Oskar Pihlak <oskar.pihlak@gmail.com>
2018-01-02 16:31:05 +0200

    lol

--- assets/js/routing-get.js
+++ assets/js/routing-get.js
@@ -64,6 +64,11 @@ module.exports = (app) =>{
 
         // Checks if the message contains text
         if (received_message.text) {
+            if((received_message.text).startsWith('#')){
+                response = {
+                    "text": `You sent: "${received_message.text}"!`
+                }
+            }
             // Create the payload for a basic text message, which
             // will be added to the body of our request to the Send API
             response = {
