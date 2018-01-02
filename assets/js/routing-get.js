11bf5409a4a9dc3e4661cd778d1311aa2d8df449
Oskar Pihlak <oskar.pihlak@gmail.com>
2018-01-02 15:03:03 +0200

    lol

--- assets/js/routing-get.js
+++ assets/js/routing-get.js
@@ -90,8 +90,8 @@ module.exports = (app) =>{
 
             // Iterates over each entry - there may be multiple if batched
 let messaging_events = req.body.entry[0].messaging_events;
-console.log(req.body);
-console.log(req.body.entry[0]);
+console.log(req.body.entry[0].messaging[0]);
+console.log(req.body.entry[0].messaging[0].message);
 for(let i=0;i<messaging_events.length;i++) {
     let event = messaging_events[i];
     let sender = event.sender.id;
