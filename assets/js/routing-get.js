f74ccc0a3417e6fa8603a5e38669891febf83119
Oskar Pihlak <oskar.pihlak@gmail.com>
2018-01-02 15:41:32 +0200

    lol

--- assets/js/routing-get.js
+++ assets/js/routing-get.js
@@ -60,6 +60,19 @@ module.exports = (app) =>{
     // Handles messages events
     function handleMessage(sender_psid, received_message) {
 
+        let response;
+
+        // Check if the message contains text
+        if (received_message.text) {
+
+            // Create the payload for a basic text message
+            response = {
+                "text": `You sent the message: "${received_message.text}". Now send me an image!`
+            }
+        }
+
+        // Sends the response message
+        callSendAPI(sender_psid, response);
     }
 
 // Handles messaging_postbacks events
@@ -69,7 +82,27 @@ module.exports = (app) =>{
 
 // Sends response messages via the Send API
     function callSendAPI(sender_psid, response) {
+        // Construct the message body
+        let request_body = {
+            "recipient": {
+                "id": sender_psid
+            },
+            "message": response
+        };
 
+        // Send the HTTP request to the Messenger Platform
+        request({
+            "uri": "https://graph.facebook.com/v2.6/me/messages",
+            "qs": { "access_token": 'EAAH0ZBiD0bXEBAJZBU6UHWyZBKk8shvJCkLjXZCTv5797tZAcxg0WrR1biZBLQZCC4W69ZAh8H3hUbu8CgeQY9PbA2ySHMGJCvhfXfsD2qhGLfenWBk2y2f5vO7GDQfO6iWeC1djqomczvbFJXi0mwCVPpDPYZAMgQVDN1YkkerVnpAZDZD' },
+            "method": "POST",
+            "json": request_body
+        }, (err, res, body) => {
+            if (!err) {
+                console.log('message sent!')
+            } else {
+                console.error("Unable to send message:" + err);
+            }
+        });
     }
 
     app.post('/bot/webhook', (req, res) => {
@@ -87,9 +120,18 @@ module.exports = (app) =>{
                 let webhook_event = entry.messaging[0];
                 console.log(webhook_event);
 
+
                 // Get the sender PSID
                 let sender_psid = webhook_event.sender.id;
                 console.log('Sender PSID: ' + sender_psid);
+
+                // Check if the event is a message or postback and
+                // pass the event to the appropriate handler function
+                if (webhook_event.message) {
+                    handleMessage(sender_psid, webhook_event.message);
+                } else if (webhook_event.postback) {
+                    handlePostback(sender_psid, webhook_event.postback);
+                }
 
             });
 
